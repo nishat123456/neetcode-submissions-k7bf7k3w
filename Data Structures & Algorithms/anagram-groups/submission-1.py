01fe6160bb407dd4ab047e53b86b786129a50542
Mustaqim Nishat <126341831+nishat123456@users.noreply.github.com>
2026-07-21 19:36:58 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #we use the dict to get the freq of all the letters
+        #whenever it is same, we group it. nice
+        #i am thinking brute force.
+
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
+
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
