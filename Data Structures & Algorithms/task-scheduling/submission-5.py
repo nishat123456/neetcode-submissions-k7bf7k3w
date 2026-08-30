2a1d6822db63c6b9f1a96b79684359d8fac6c552
Mustaqim Nishat <126341831+nishat123456@users.noreply.github.com>
2026-08-30 12:31:18 -0500

    Add: task-scheduling - submission-5

---
+++ Data Structures & Algorithms/task-scheduling/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        #n is the cooldown period
+        #we make the count of tasks
+        #we make max heap and have the freq in it.
+        #we make queue to keep track of who is coming when
+        count = Counter(tasks)
+        maxHeap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxHeap)
+        q = deque() 
+        time = 0
+
+        while maxHeap or q:
+            time += 1
+
+            if not maxHeap:
+                time = q[0][1]
+            else:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, time + n])
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time
+
+
