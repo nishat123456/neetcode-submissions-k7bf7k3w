6aa1733bbc466c53693d174d8960ca8477b4afb5
Mustaqim Nishat <126341831+nishat123456@users.noreply.github.com>
2026-09-10 19:25:37 -0500

    Add: is-subsequence - submission-5

---
+++ Data Structures & Algorithms/is-subsequence/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        #we we go one letter by one letter.
+        #one pointer goes from s, one from t. T moves on as long as it doesn't find the char in s. we could use queue and do popleft. Think this is optimized. What is the brute force one?
+        l = 0
+        for r in range(len(t)):
+            if s and t[r] == s[l]:
+                l += 1
+
+            if l == (len(s)):
+                return True
+            
+        return False
+
+        #time O(N)
+        #space O(1)
